7f283ea35b43005918bc9723513d3f9ff8c4b98b
Luka Horvat <lukahorvat9@gmail.com>
2013-09-09 21:00:22 +0200

    Test commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var log = function (str) {
 	fs.appendFileSync("log", str + "\n");
 }
 
-log("New version of GitHubRunner: build 13");
+log("New version of GitHubRunner: build 14");
 
 app.post("/push", function (req, res) {
 	var info = JSON.parse(req.param("payload"));
